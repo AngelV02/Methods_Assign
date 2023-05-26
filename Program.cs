93e61af8c3d1ceb5b542d073097f40e784733122
AngelV02 <125764846+AngelV02@users.noreply.github.com>
2023-05-26 10:38:49 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Methods_Assign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int userInput = int.Parse(Console.ReadLine());
+
+            // Create an instance of MathOperations class
+            MathOperations mathOperations = new MathOperations();
+
+            // Call each method and display the returned integer
+            int doubledNumber = mathOperations.DoubleNumber(userInput);
+            Console.WriteLine($"Double of {userInput} is: {doubledNumber}");
+
+            int squaredNumber = mathOperations.SquareNumber(userInput);
+            Console.WriteLine($"Square of {userInput} is: {squaredNumber}");
+
+            int factorial = mathOperations.CalculateFactorial(userInput);
+            Console.WriteLine($"Factorial of {userInput} is: {factorial}");
+        }
+    }
+}
